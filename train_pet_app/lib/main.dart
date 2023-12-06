f87065cf4a2c7b4688723bedb2dc9c50ed11e46b
viniciusgomes2005 <128643734+viniciusgomes2005@users.noreply.github.com>
2023-12-06 18:32:40 -0300

    .

--- train_pet_app/lib/main.dart
+++ train_pet_app/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MaterialApp(home: MyApp(),));
 }
